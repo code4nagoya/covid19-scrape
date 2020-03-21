3cf27be6ac4190a58cc3a9c3fa6862fd8799b0df
Miura55 <acordion.piano@gmail.com>
2020-03-22 08:29:17 +0900

    create data folder

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ del df1['発表日 年代・性別'], df1['Unnamed: 0']
 df1["発表日"] = data1[0]
 df1["年代・性別"] = data1[1]
 
-# 2ページ以降はデータがカラムがないので全部結合している
+# 2ページ以降はデータにカラムがないので全部結合している
 dfs = tabula.read_pdf(FILE_PATH, stream=True, pages="2-{}".format(pages), pandas_options={'header': None})
 df2 = pd.concat(dfs, ignore_index=True)
 data2 = df2[1].str.split(' ', expand=True)
@@ -25,11 +25,11 @@ df2[7] = data2[0]
 df2[8] = data2[1]
 df2.columns = df1.columns
 
-# 1ページ目と２ページ目以降を結合
+# 1ページ目と2ページ目以降を結合
 df = pd.concat([df1, df2], ignore_index=True)
 df.index = df["No"]
 del df["No"]
 
-# CSVに書き込み
-df.to_csv("data1.csv")
+# CSVに書き込み(ファイル名はdata1.py)
+df.to_csv("data/data1.csv")
 print(df)
