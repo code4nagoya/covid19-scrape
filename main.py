ccdb09aa7e31c13152b7dd38603bc3932cea4ef8
Miura55 <acordion.piano@gmail.com>
2020-03-26 23:56:30 +0900

    Adde sequense

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ def add_date(df):
     df["発表日"] = ["2020年" + date for date in df["発表日"]]
     basedate = pd.to_datetime(df["発表日"], format="%Y年%m月%d日")
     df["date"] = basedate.dt.strftime("%Y-%m-%d")
-    df["short_date"] = basedate.dt.strftime("%m/%d")
+    df["short_date"] = basedate.dt.strftime("%m\\/%d")
     df["w"] = [int(w)+1 if int(w)+1 !=7 else 0 for w in basedate.dt.dayofweek]
     return df
 
